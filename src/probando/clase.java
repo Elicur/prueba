0c822791c8309366ba20b1fb723151f77c1e2827
Elicur <elias.bentancur@estudiantes.utec.edu.uy>
2023-08-22 20:31:27 -0300

    add int en clase

--- src/probando/clase.java
+++ src/probando/clase.java
@@ -2,8 +2,10 @@ package probando;
 
 public class clase {
 	private String unAtributo;
+	private Integer funca;
 	
-	public clase (String aaa) {
+	public clase (String aaa, Integer funca) {
 		this.unAtributo = aaa;
+		this.funca = funca;
 	}
 }
