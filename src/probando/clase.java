8659c4c97eececf7cc91a152531351ad9e79a604
Elicur <elias.bentancur@estudiantes.utec.edu.uy>
2023-08-22 20:28:38 -0300

    aber2

--- src/probando/clase.java
+++ src/probando/clase.java
@@ -3,8 +3,11 @@ package probando;
 public class clase {
 	private String unAtributo;
 	private String otroAtributo;
+	private String unoMas;
 	
-	public clase (String aaa, String oa) {
+	public clase (String aaa, String oa, String um) {
 		this.unAtributo = aaa;
-		this.otroAtributo = oa;	}
+		this.otroAtributo = oa;
+		this.unoMas = um;
+	}
 }
